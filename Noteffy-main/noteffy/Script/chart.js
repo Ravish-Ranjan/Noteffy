2651a1476001da4f8b84c9de6ab9cffea1207176
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2023-02-28 23:15:16 +0530

    added data visualization function

--- Noteffy-main/noteffy/Script/chart.js
+++ Noteffy-main/noteffy/Script/chart.js
@@ -1,23 +1,47 @@
+function calc_completed_task(jsonData) {
+  let date = [];
+  let count = [0];
+  for (i = 0; i < jsonData.length; i++){
+    if (!(date.includes(jsonData[i]['Date']))) {
+      date.push(jsonData[i]['Date']);
+    }
+    count[i] = 0;
+  }
+  for (i = 0; i < jsonData.length; i++){
+    if (date.includes(jsonData[i]['Date']))
+      count[date.indexOf(jsonData[i]['Date'])]++;
+  }
+  var obj = {
+    "date": date,
+    "count" :count
+  }
+  return obj;
+}
+
+
 const ctx = document.getElementById('context');
 setTimeout(()=>{
   document.getElementById("load").remove();
   ctx.style.display = "block";
 },4000)
-new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+fetch("../data/task.json").then((res) => res.json()).then((json) => {
+  let obj = calc_completed_task(json);
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: obj.date,
+      datasets: [{
+        label: 'Number of tasks completed',
+        data: obj.count,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false
+        }
       }
     }
-  }
-});
+  });
+})
