9726bb63d49b0902afade7632fcae5065234eb90
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2023-03-20 19:53:30 +0530

    Removed redundant noteffy folders

--- Noteffy-main/noteffy/Script/note.js
+++
@@ -1,5 +0,0 @@
-function getContent(task_no) {
-    let content = document.getElementById("content" + task_no);
-    navigator.clipboard.writeText(content.innerText);
-    message("coppied to clipboard", "message_success");
-}
