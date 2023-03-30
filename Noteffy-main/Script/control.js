4e305c11955a7998f9433be26f9954dbec534614
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2023-03-30 20:50:23 +0530

    changes to control.html

---
+++ Noteffy-main/Script/control.js
@@ -0,0 +1,30 @@
+function openTab(evt, tabname) { // this function is used to move arround the tabs in the main page
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("main");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tbs");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+    divs = document.getElementsByClassName("main")
+}
+let name = async () => {
+    let user_name = "User Name";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    decodedCookie = decodedCookie.split(";");
+    Array.from(decodedCookie).map((ele) => {
+        let temp = ele.split("=");
+        if (temp[0].trim() == "user") {
+            user_name = temp[1].trim();
+        }
+    })
+    user = await fetch(window.location.href.split("/HTML/control.html")[0] + "/php/encrypt_cookie.php?" + (new URLSearchParams({ value: user_name })), { method: "GET", mode: "cors" });
+    user = await user.json();
+    user = user['res'];
+    document.getElementById("admin-name").innerText = user;
+}
+name();
