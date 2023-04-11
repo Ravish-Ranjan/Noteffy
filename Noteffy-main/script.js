edb7b5c8791dc3ae1f4a5a5f579106d5a127a445
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2023-04-11 19:35:08 +0530

    removed manifest.json

--- Noteffy-main/script.js
+++
@@ -1,7 +0,0 @@
-async function api(){
-    let response = await fetch("data/Details.json");
-    response = await response.json();
-    let rand = Math.floor(Math.random() * 2);
-    document.getElementById("content").innerText = response["Users"][rand]["Email"];
-}
-api();
